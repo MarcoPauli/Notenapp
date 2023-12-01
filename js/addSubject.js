1721674b06596510e7207130e0b22b1a2e92011e
aliquipirata <103878112+MarcoPauli@users.noreply.github.com>
2023-12-01 10:30:00 +0100

    Update addSubject.js

--- js/addSubject.js
+++ js/addSubject.js
@@ -1,7 +1,15 @@
+
+/*
+THIS FILE HAS THE TASK TO ADD FILES INCLUDING THE FUNCTIONS "STARTTOADDSUBJECTS" AND "ADDPARTICULARSUBJECT" AND THE CONSTRUCTOR "CREATESUBJECT" AS WELL WHICH CAN BE USED IN OTHER FILES SINCE IT IS EXPORTED
+/*
+
+//All necessary functions are imported from other files including functions "showinformation", "checkid" and "showsinglehtmlelement"
 import { showInformation } from "./notification.js";
 import { checkID } from "./checkId.js";
 import { showSingleHTMLElement } from "./main.js";
+//End of all necessary imports
 
+//This adds an event on starttoaddsubjectbutton so you can add an subject by clicking on this button
 addSubjectBtn.addEventListener("click", startToAddSubject);
 
 function startToAddSubject() {
@@ -18,8 +26,6 @@ function startToAddSubject() {
     if((userInput == "") || (userInput.includes(" "))) showInformation("invalide Eingabe", "red")
     userValue.value = "";
 }
-
-
 
 function addParticularSubject(name) {
     subjects[name] = new CreateSubject(name);
