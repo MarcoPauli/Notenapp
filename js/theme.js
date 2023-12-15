1351bce99786645e40b595197097e7f9da8363e9
aliquipirata <103878112+MarcoPauli@users.noreply.github.com>
2023-12-15 21:12:48 +0100

    update15122023#2

--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,16 @@
 //The task of the function "userBackgroundColor" is to check which background color is currently being used by the system and, as soon as these settings are changed, adjust the app settings accordingly.
 export function userBackgroundColor() {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        changeToXMode("white", "rgb(80, 80, 80)", "rgba(80, 80, 80, 0.5)", "rgb(100, 100, 100)");
+        changeToXMode("white", "rgb(70, 70, 70)", "rgba(80, 80, 80, 0.5)", "rgb(100, 100, 100)", "rgb(90, 90, 90)", "rgb(100, 100, 100)");
     }
     if (!(window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        changeToXMode("black", "rgb(204,238,247)", "rgba(100, 100, 100, 0.5)", "white");
+        changeToXMode("black", "rgb(204,238,247)", "rgba(100, 100, 100, 0.5)", "white", "rgb(255, 255, 255)", "rgb(206, 201, 201)");
     }
 }
 //End of function "userBackgroundColor"
 
 //This function changes to xmode
-function changeToXMode(color, backgroundColor, overlayDivBgColor, tbColor) {
+function changeToXMode(color, backgroundColor, overlayDivBgColor, tbColor, c1, c2) {
     let overlayDiv = document.getElementById("overlayDiv");
     let body = document.querySelector("body");
     let td = document.querySelectorAll("td");
@@ -29,26 +29,18 @@ function changeToXMode(color, backgroundColor, overlayDivBgColor, tbColor) {
     let averageAllSubjectsDiv = document.getElementById("averageAllSubjectsDiv");
     averageAllSubjectsDiv.style.color = "white";
     statistics.style.backgroundColor = tbColor;
-    //changeTableTheme(tr, tbColor, color);
-    //changeTableTheme(th, tbColor, color);
+    changeTableTheme(tr, tbColor, color, c1, c2);
 }
 //End of function "changeToXMode"
 
 //Start of function changeTableTheme
-function changeTableTheme(x, tbColor, color) {
+function changeTableTheme(x, tbColor, color, c1, c2) {
     for (let i = 2; i < x.length;) {
-        x[i].style.backgroundColor = color;
-        x[i].style.color = tbColor;
+        x[i].style.backgroundColor = c2;
+        x[i].style.color = color;
+        x[i - 1].style.backgroundColor = c1;
+        x[i - 1].style.color = color;
         i = i + 2;
     }
 }
 //End of function changeTableTheme
-
-/*
-function changeTableTheme(x, tbColor, color) {
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.backgroundColor = tbColor;
-        x[i].style.color = color;
-    }
-}
-*/
